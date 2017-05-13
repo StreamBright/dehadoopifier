1deaa82e077145d419e87a7a095270ee066cb7d9
Istvan Szukacs <istvan@streambrightdata.com>
2017-05-13 09:49:11 +0200

    Removing tabs

--- src/dehadoopifier.ml
+++ src/dehadoopifier.ml
@@ -24,54 +24,54 @@ let walk_directory_tree dir pattern =
 
 (* Reads a line from a channel and returns Some(line) or None if file end has reached *)
 let read_line i =
-	try
-		Some (input_line i)
-	with End_of_file ->
-		None
+  try
+    Some (input_line i)
+  with End_of_file ->
+    None
 
 (* Get lines from a file, closes file after read it *)
 let lines_from_file filename =
-	let file_in = open_in filename in
-	let clean_up channel_in = close_in channel_in in
+  let file_in = open_in filename in
+  let clean_up channel_in = close_in channel_in in
   let rec lines_from_file_aux in_channel acc =
-		match (read_line in_channel) with
-    	| None ->
-				clean_up file_in;
-				List.rev acc
-    	| Some s ->
-					lines_from_file_aux in_channel (s :: acc) in
+    match (read_line in_channel) with
+      | None ->
+        clean_up file_in;
+        List.rev acc
+      | Some s ->
+          lines_from_file_aux in_channel (s :: acc) in
   lines_from_file_aux file_in []
 
 (* Package line regexp (what could possibly go wrong? :)) *)
 let package_regexp =
-	Str.regexp ".*package.*"
+  Str.regexp ".*package.*"
 
 (* Same for import as above*)
 let import_regexp =
-	Str.regexp ".*import.*"
+  Str.regexp ".*import.*"
 (* Finding regexp in a string starting from the 0th position *)
 let find_matches regxp str =
-	Str.string_match regxp str 0
+  Str.string_match regxp str 0
 
 let replace input output =
   Str.global_replace (Str.regexp_string input) output
 
 let remove_package package =
-	replace "package " "" package
+  replace "package " "" package
 
 let remove_import import =
   replace "import " "" import
 
 let remove_semi_colon s =
-	replace ";" "" s
+  replace ";" "" s
 
 let process_package package =
-	let s = remove_package package in
-	remove_semi_colon s
+  let s = remove_package package in
+  remove_semi_colon s
 
 let process_import import =
-	let s = remove_import import in
-	remove_semi_colon s
+  let s = remove_import import in
+  remove_semi_colon s
 
 let get_package file_name =
   process_package
@@ -81,20 +81,20 @@ let get_package file_name =
     ) (lines_from_file file_name)))
 
 let get_imports file_name =
-	(List.map process_import (List.filter (fun s -> find_matches import_regexp s) (lines_from_file file_name)))
+  (List.map process_import (List.filter (fun s -> find_matches import_regexp s) (lines_from_file file_name)))
 
 let get_name file_name sep =
-	sep ^ "/" ^ (List.hd (List.tl (Str.split (Str.regexp ("/" ^ sep ^ "/")) file_name)))
+  sep ^ "/" ^ (List.hd (List.tl (Str.split (Str.regexp ("/" ^ sep ^ "/")) file_name)))
 
 (*
-	Getting 3 element tuples with (package name, file, imports)
+  Getting 3 element tuples with (package name, file, imports)
 *)
 let get_triplets files =
-	List.map (
+  List.map (
     fun file_name -> (
       get_package file_name,
       get_name file_name "com",
-			get_imports file_name
+      get_imports file_name
     )
   ) files
 
@@ -106,10 +106,10 @@ let pp_triplets (package, file_name, imports) =
   Printf.printf "(%s, %s, [%s])\n" package file_name (process_imports imports)
 
 let split_package package =
-	Str.split (Str.regexp "\\.") package
+  Str.split (Str.regexp "\\.") package
 
 let package_to_path package =
-	String.concat "/" (split_package package)
+  String.concat "/" (split_package package)
 
 let main =
   let dir = Sys.argv.(1) in
