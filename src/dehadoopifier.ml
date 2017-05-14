6f372735280a12055df16b837ab7556cee63d90b
Istvan Szukacs <istvan@streambrightdata.com>
2017-05-14 15:57:08 +0200

    Adding search for packages

--- src/dehadoopifier.ml
+++ src/dehadoopifier.ml
@@ -42,13 +42,13 @@ let lines_from_file filename =
           lines_from_file_aux in_channel (s :: acc) in
   lines_from_file_aux file_in []
 
-(* Package line regexp (what could possibly go wrong? :)) *)
+(* Package line regexp (what could possibly go wrong? :) *)
 let package_regexp =
   Str.regexp ".*package.*"
 
-(* Same for import as above*)
+(* Same for import as above, needs to be better*)
 let import_regexp =
-  Str.regexp ".*import.*"
+  Str.regexp ".*import\\ [a-zA-Z]+\\."
 (* Finding regexp in a string starting from the 0th position *)
 let find_matches regxp str =
   Str.string_match regxp str 0
@@ -83,8 +83,8 @@ let get_package file_name =
 let get_imports file_name =
   (List.map process_import (List.filter (fun s -> find_matches import_regexp s) (lines_from_file file_name)))
 
-let get_name file_name sep =
-  sep ^ "/" ^ (List.hd (List.tl (Str.split (Str.regexp ("/" ^ sep ^ "/")) file_name)))
+let get_class file_name =
+  (List.hd (List.rev (List.tl (Str.split (Str.regexp ("/")) file_name))))
 
 (*
   Getting 3 element tuples with (package name, file, imports)
@@ -93,7 +93,7 @@ let get_triplets files =
   List.map (
     fun file_name -> (
       get_package file_name,
-      get_name file_name "com",
+      get_class file_name,
       get_imports file_name
     )
   ) files
@@ -111,9 +111,61 @@ let split_package package =
 let package_to_path package =
   String.concat "/" (split_package package)
 
+let paths =
+  let ht = Hashtbl.create 10 in
+  Hashtbl.add ht "org_apache_hadoop_hive_ql" "../hive/ql/src/java";
+  ht
+
+let ht_lookup ht key =
+	try Some (Hashtbl.find ht key)
+  with Not_found -> None
+
+let search_package_path full_path =
+	let rec sp path_rev =
+	let lookup = ht_lookup paths (String.concat "_" (List.rev path_rev)) in
+	match path_rev, lookup
+		with
+			| head::tail, None -> sp tail
+			| _, Some found -> Some found
+			| head, None -> None in
+	sp (List.rev full_path)
+
+let package_from_import import =
+  let full_path_as_list = List.rev (List.tl (List.tl (List.rev (Str.split (Str.regexp "\\.") import)))) in
+  let package_path = search_package_path full_path_as_list in
+  package_path
+(*
+
+results:
+
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/types/WebPageView.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/types/WebPageViewRow.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/serde/out/OrcSerializer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/serde/out/ParquetSerializer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/serde/out/Serializer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/serde/in/AvroDeserializer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/serde/in/Deserializer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/io/EventProcessor.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/io/EventProcessorJob.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/io/S3Uploader.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/io/S3UploaderJob.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/http/handlers/ConfigHandler.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/http/handlers/EchoHandler.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/http/handlers/EventHandler.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/http/handlers/LoginHandler.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/http/HttpServer.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/config/Defaults.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/aaa/Auth.java
+../reactively/server/java/jflow/src/main/java/com/streambright/jflow/App.java
+
+
+*)
 let main =
   let dir = Sys.argv.(1) in
   let results = walk_directory_tree dir ".*\\.java" in
+	print_endline "Results :: ";
+	List.iter print_endline results;
+	print_endline "Triplets  :: ";
   List.iter pp_triplets (get_triplets results)
 
 let () =
